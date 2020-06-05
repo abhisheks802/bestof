6143bca68aa4b469dc5de48f6b85fae2ca9544e2
Abhishek Singh <42760946+abhisheks802@users.noreply.github.com>
2020-06-05 12:24:00 +0530

    Add files via upload

---
+++ rootfolder/js/main.js
@@ -0,0 +1,189 @@
+$(document).ready(function(){
+    $("#about-wrapper, #names-wrapper").click(function () {
+        $("#about-wrapper, #names-wrapper").fadeToggle();
+    });
+    $("#more").click(function () {
+       $(".others").fadeToggle(400);
+    });
+    $("#bollywood, #hollywood, #series, #anime").click(function () {
+        $(".others").fadeOut(200);
+    });
+    $("#bname1, #bmovie-detail1").click(function () {
+       $("#b-wrapper2, #b-wrapper3, #b-wrapper4, #b-wrapper5, #b-wrapper6, #b-wrapper7, #b-wrapper8, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail1").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname2, #bmovie-detail2").click(function () {
+       $("#b-wrapper1, #b-wrapper3, #b-wrapper4, #b-wrapper5, #b-wrapper6, #b-wrapper7, #b-wrapper8, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail2").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname3, #bmovie-detail3").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper4, #b-wrapper5, #b-wrapper6, #b-wrapper7, #b-wrapper8, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail3").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname4, #bmovie-detail4").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper3, #b-wrapper5, #b-wrapper6, #b-wrapper7, #b-wrapper8, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail4").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname5, #bmovie-detail5").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper3, #b-wrapper4, #b-wrapper6, #b-wrapper7, #b-wrapper8, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail5").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname6, #bmovie-detail6").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper3, #b-wrapper4, #b-wrapper5, #b-wrapper7, #b-wrapper8, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail6").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname7, #bmovie-detail7").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper3, #b-wrapper4, #b-wrapper5, #b-wrapper6, #b-wrapper8, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail7").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname8, #bmovie-detail8").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper3, #b-wrapper4, #b-wrapper5, #b-wrapper6, #b-wrapper7, #b-wrapper9, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail8").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname9, #bmovie-detail9").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper3, #b-wrapper4, #b-wrapper5, #b-wrapper6, #b-wrapper7, #b-wrapper8, #b-wrapper10").fadeToggle();
+       $("#bmovie-detail9").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#bname10, #bmovie-detail10").click(function () {
+       $("#b-wrapper1, #b-wrapper2, #b-wrapper3, #b-wrapper4, #b-wrapper5, #b-wrapper6, #b-wrapper7, #b-wrapper8, #b-wrapper9").fadeToggle();
+       $("#bmovie-detail10").fadeToggle();
+       window.location.href="#b-whole";
+    });
+    $("#hname1, #hmovie-detail1").click(function () {
+       $("#h-wrapper2, #h-wrapper3, #h-wrapper4, #h-wrapper5, #h-wrapper6, #h-wrapper7, #h-wrapper8, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail1").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname2, #hmovie-detail2").click(function () {
+       $("#h-wrapper1, #h-wrapper3, #h-wrapper4, #h-wrapper5, #h-wrapper6, #h-wrapper7, #h-wrapper8, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail2").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname3, #hmovie-detail3").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper4, #h-wrapper5, #h-wrapper6, #h-wrapper7, #h-wrapper8, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail3").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname4, #hmovie-detail4").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper3, #h-wrapper5, #h-wrapper6, #h-wrapper7, #h-wrapper8, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail4").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname5, #hmovie-detail5").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper3, #h-wrapper4, #h-wrapper6, #h-wrapper7, #h-wrapper8, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail5").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname6, #hmovie-detail6").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper3, #h-wrapper4, #h-wrapper5, #h-wrapper7, #h-wrapper8, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail6").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname7, #hmovie-detail7").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper3, #h-wrapper4, #h-wrapper5, #h-wrapper6, #h-wrapper8, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail7").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname8, #hmovie-detail8").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper3, #h-wrapper4, #h-wrapper5, #h-wrapper6, #h-wrapper7, #h-wrapper9, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail8").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname9, #hmovie-detail9").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper3, #h-wrapper4, #h-wrapper5, #h-wrapper6, #h-wrapper7, #h-wrapper8, #h-wrapper10").fadeToggle();
+       $("#hmovie-detail9").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#hname10, #hmovie-detail10").click(function () {
+       $("#h-wrapper1, #h-wrapper2, #h-wrapper3, #h-wrapper4, #h-wrapper5, #h-wrapper6, #h-wrapper7, #h-wrapper8, #h-wrapper9").fadeToggle();
+       $("#hmovie-detail10").fadeToggle();
+       window.location.href="#h-whole";
+    });
+    $("#sname1, #smovie-detail1").click(function () {
+       $("#s-wrapper2, #s-wrapper3, #s-wrapper4, #s-wrapper5, #s-wrapper6, #s-wrapper7, #s-wrapper8, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail1").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname2, #smovie-detail2").click(function () {
+       $("#s-wrapper1, #s-wrapper3, #s-wrapper4, #s-wrapper5, #s-wrapper6, #s-wrapper7, #s-wrapper8, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail2").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname3, #smovie-detail3").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper4, #s-wrapper5, #s-wrapper6, #s-wrapper7, #s-wrapper8, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail3").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname4, #smovie-detail4").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper3, #s-wrapper5, #s-wrapper6, #s-wrapper7, #s-wrapper8, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail4").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname5, #smovie-detail5").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper3, #s-wrapper4, #s-wrapper6, #s-wrapper7, #s-wrapper8, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail5").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname6, #smovie-detail6").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper3, #s-wrapper4, #s-wrapper5, #s-wrapper7, #s-wrapper8, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail6").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname7, #smovie-detail7").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper3, #s-wrapper4, #s-wrapper5, #s-wrapper6, #s-wrapper8, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail7").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname8, #smovie-detail8").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper3, #s-wrapper4, #s-wrapper5, #s-wrapper6, #s-wrapper7, #s-wrapper9, #s-wrapper10").fadeToggle();
+       $("#smovie-detail8").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname9, #smovie-detail9").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper3, #s-wrapper4, #s-wrapper5, #s-wrapper6, #s-wrapper7, #s-wrapper8, #s-wrapper10").fadeToggle();
+       $("#smovie-detail9").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#sname10, #smovie-detail10").click(function () {
+       $("#s-wrapper1, #s-wrapper2, #s-wrapper3, #s-wrapper4, #s-wrapper5, #s-wrapper6, #s-wrapper7, #s-wrapper8, #s-wrapper9").fadeToggle();
+       $("#smovie-detail10").fadeToggle();
+       window.location.href="#s-whole";
+    });
+    $("#aname1, #amovie-detail1").click(function () {
+       $("#a-wrapper2, #a-wrapper3, #a-wrapper4, #a-wrapper5").fadeToggle();
+       $("#amovie-detail1").fadeToggle();
+       window.location.href="#a-whole";
+    });
+    $("#aname2, #amovie-detail2").click(function () {
+       $("#a-wrapper1, #a-wrapper3, #a-wrapper4, #a-wrapper5").fadeToggle();
+       $("#amovie-detail2").fadeToggle();
+       window.location.href="#a-whole";
+    });
+    $("#aname3, #amovie-detail3").click(function () {
+       $("#a-wrapper1, #a-wrapper2, #a-wrapper4, #a-wrapper5").fadeToggle();
+       $("#amovie-detail3").fadeToggle();
+       window.location.href="#a-whole";
+    });
+    $("#aname4, #amovie-detail4").click(function () {
+       $("#a-wrapper1, #a-wrapper2, #a-wrapper3, #a-wrapper5").fadeToggle();
+       $("#amovie-detail4").fadeToggle();
+       window.location.href="#a-whole";
+    });
+    $("#aname5, #amovie-detail5").click(function () {
+       $("#a-wrapper1, #a-wrapper2, #a-wrapper3, #a-wrapper4").fadeToggle();
+       $("#amovie-detail5").fadeToggle();
+       window.location.href="#a-whole";
+    });
+    $("h3, .movie-detail").click(function() {
+    $("h3").toggleClass("active");
+  });
+});
